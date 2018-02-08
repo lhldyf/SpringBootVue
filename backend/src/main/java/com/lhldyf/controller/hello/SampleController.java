8209baabb0053bdc7115cabeb663e1ce61d98ca8
lhldyf <lhldyf45@gmail.com>
2018-02-08 16:34:58 +0800

    test

--- backend/src/main/java/com/lhldyf/controller/hello/SampleController.java
+++ backend/src/main/java/com/lhldyf/controller/hello/SampleController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,7 +16,7 @@ public class SampleController {
 	@RequestMapping("/")
 	@ResponseBody
 	String home() {
-		return "Hello World!";
+		return "Hello World~";
 	}
 
 	@RequestMapping("/user/list")
